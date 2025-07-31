d41c63d6fdd87247d8c253094920fb8c6d5a1936
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-07-31 16:21:32 -0300

    feat: reunião dos dados a serem enviados na rede para a questão 2

--- src/main.py
+++ src/main.py
@@ -6,3 +6,9 @@ cpus_fisicas = psutil.cpu_count(logical = False)
 cpus_logicas = psutil.cpu_count(logical = True)
 threads_por_nucleo_fisico = cpus_logicas / cpus_fisicas
 print(cpus_fisicas, cpus_logicas, threads_por_nucleo_fisico)
+
+# 2 - Memória RAM livre
+tupla_da_ram = psutil.virtual_memory()
+memoria_ram_imediata = tupla_da_ram[1]
+memoria_ram_alocada_e_nao_usada = tupla_da_ram[4]
+print(tupla_da_ram, memoria_ram_imediata, memoria_ram_alocada_e_nao_usada)
