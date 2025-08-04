dc6de8c353988043654bfaad7c5510e58eb20809
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-08-04 09:14:32 -0300

    feat: consolidando informações em um dict

--- src/main.py
+++ src/main.py
@@ -15,15 +15,30 @@ print(tupla_da_ram, memoria_ram_imediata, memoria_ram_alocada_e_nao_usada)
 
 # 3 - Espaço livre em disco
 particoes_de_disco = psutil.disk_partitions(all = False)
-memoria_livre_de_cada_particao = list()
-
-for particao in particoes_de_disco:
+informacoes_das_particoes = []
+particoes = psutil.disk_partitions(all=False)
+for particao in particoes:
     try:
-        memoria_da_particao = psutil.disk_usage(particao.mountpoint)
-        memoria_livre_de_cada_particao.append({
-            "particao": particao.mountpoint,
-            "livre": memoria_da_particao.free
+        uso = psutil.disk_usage(particao.mountpoint)
+        informacoes_das_particoes.append({
+            "device": particao.device,
+            "mountpoint": particao.mountpoint,
+            "fstype": particao.fstype,
+            "total_bytes": uso.total,
+            "used_bytes": uso.used,
+            "free_bytes": uso.free,
+            "percent_used": uso.percent
         })
     except Exception as e:
-        print(f"Não foi possível obter as informações sobre o ponto de montagem {particao.mountpoint}: {e}")
-print(memoria_livre_de_cada_particao)
+        print(f"Não foi possível obter o uso do disco para {partition.mountpoint}: {e}")
+
+informacoes_do_sistema = {}
+informacoes_do_sistema["processadores"] = {
+    "logicos": cpus_logicas,
+    "fisicos": cpus_fisicas
+}
+informacoes_do_sistema["ram"] = {
+    "imediata": memoria_ram_imediata,
+    "alocada_e_nao_usada": memoria_ram_alocada_e_nao_usada
+}
+informacoes_do_sistema["particoes"] = informacoes_das_particoes
